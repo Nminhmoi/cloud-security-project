2647a134f9970219c133d41f803136cdf0c1f65d
hoangkhanhnguyen <hongkhanhnguyen1602@gmail.com>
2026-08-31 22:12:16 +0700

    feat: khoi tao ha tang terraform tren aws

---
+++ terraform/variables.tf
@@ -0,0 +1,36 @@
+variable "aws_region" {
+  type    = string
+  default = "ap-southeast-1"
+}
+
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Access Key ID"
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Secret Access Key"
+  sensitive   = true
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  type    = string
+  default = "10.0.1.0/24"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro" 
+}
